b95430b6937cf6261ba2ee6554c5db0d4b24f6da
clearloop <udtrokia@gmail.com>
2020-12-03 14:36:01 +0800

    feat(trap): trap debugger

--- crates/trap/build.rs
+++ crates/trap/build.rs
@@ -2,7 +2,7 @@
 
 use std::{env, fs, path::Path, process::Command};
 
-fn panic() -> Vec<u8> {
+fn panic() -> String {
     let manifest = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let trap = Path::new(&manifest);
     Command::new("cargo")
@@ -16,20 +16,24 @@ fn panic() -> Vec<u8> {
         .status()
         .unwrap();
 
-    fs::read(trap.join("../panic/target/wasm32-unknown-unknown/debug/panic.wasm")).unwrap()
+    fs::canonicalize(trap.join("../panic/target/wasm32-unknown-unknown/debug/panic.wasm"))
+        .unwrap()
+        .to_string_lossy()
+        .to_string()
 }
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("panic.rs");
+    let wasm = panic();
     fs::write(
         &dest_path,
         format!(
             "\
             ///The WASM binary of panic function \n\
-            pub const PANIC_WASM: &[u8] = &{:?};\
+            pub const PANIC_WASM: &str = {:?};\
             ",
-            &panic(),
+            wasm,
         ),
     )
     .unwrap();
--- crates/trap/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! Trap wasm
-include!(concat!(env!("OUT_DIR"), "/panic.rs"));
-
-fn main() {
-    println!("{:?}", PANIC_WASM);
-}
